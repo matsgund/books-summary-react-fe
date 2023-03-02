4d8e342b68ea48afde4bdfb17131dab9b723ba10
Mats Gundersen <matsgundersen@hotmail.com>
2023-03-02 14:02:00 +0100

    change

--- src/pages/homepage/HomePage.test.tsx
+++ src/pages/homepage/HomePage.test.tsx
@@ -1,26 +1,16 @@
 import {describe, test, expect} from 'vitest';
 import {render, screen} from '@testing-library/react';
-import HomePage from './HomePage';
-import { MemoryRouter } from 'react-router-dom';
+import HomePageBooks from './components/homepageBooks/homePageBooks';
 
 describe('HomePageBooks', () => {
 
-    test('Should always display homepage titles"', () => {
-        render(
-            <MemoryRouter>
-                <HomePage />
-            </MemoryRouter>
-        );
-        expect(screen.getByText(/The Summary Hub/)).toBeDefined();
+    test('should render books or error message if unable to fetch recent bookss', () => {
+        expect(screen.findAllByText(/The Summary Hub/)).toBeDefined();
     });
 
-    test('should give error unable to load books', () => {
-        render(
-            <MemoryRouter>
-                <HomePage />
-            </MemoryRouter>
-        );
-        expect(screen.getByText(/Unable to load books/)).toBeDefined();
+    test('should render books or error message if unable to fetch recent books', () => {
+        render(<HomePageBooks />);
+        expect(screen.findAllByText(/Recently added:|Unable to load books/)).toBeDefined();
     });
     }
 );
