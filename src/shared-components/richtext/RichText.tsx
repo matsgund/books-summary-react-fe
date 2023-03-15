27e4e675f6cbcd6d16c8db3bf1f41608045fd902
Mats Gundersen <matsgundersen@hotmail.com>
2023-03-15 13:48:45 +0100

    added styling to li and ul elements in reachTextComponent

--- src/shared-components/richtext/RichText.tsx
+++ src/shared-components/richtext/RichText.tsx
@@ -2,8 +2,9 @@ import {PortableText} from '@portabletext/react'
 import urlBuilder from '@sanity/image-url'
 import {getImageDimensions} from '@sanity/asset-utils'
 import client from '@/utils/sanityClient';
+import  { ReactNode } from 'react';
 
-interface ImageComponentProps {
+interface ComponentProps {
     value: any;
     isInline: boolean;
 }
@@ -13,33 +14,51 @@ interface ImageDimensions {
     height: number;
 }
 
-const ImageComponent = ({value, isInline} : ImageComponentProps) => {
+interface Components {
+    types: {
+        image: (props: ComponentProps) => JSX.Element;
+    };
+    list: {
+        bullet: (props: { children: ReactNode }) => JSX.Element;
+    };
+    listItem: {
+        bullet: (props: { children: ReactNode }) => JSX.Element;
+    };
+}
+
+const ImageComponent = ({value, isInline} : ComponentProps) => {
     const {width, height} : ImageDimensions = getImageDimensions(value)
-    return (
-        <img
-        src={urlBuilder(client)
-            .image(value)
-            .auto('format')
-            .url()}
-        alt={value.alt || ' '}
-        loading="lazy"
-        style={{
-            display: isInline ? 'inline-block' : 'block',
-            maxWidth: '100%',
-            aspectRatio: width / height,
-        }}
-        />
-    )
-    }
-    
+        return (
+            <img
+            src={urlBuilder(client)
+                .image(value)
+                .auto('format')
+                .url()}
+            alt={value.alt || ' '}
+            loading="lazy"
+            style={{
+                display: isInline ? 'inline-block' : 'block',
+                maxWidth: '100%',
+                aspectRatio: width / height,
+            }}
+            />
+        )
+}
+
 const components = {
     types: {
         image: ImageComponent,
     },
+    list: {
+        bullet: ({children} : { children: ReactNode } ) => <ul style={{listStyleType: 'auto'}}>{children}</ul>,
+    },
+    listItem: {
+        bullet: ({children} : { children: ReactNode }) => <li style={{marginBottom: 10}}>{children}</li>,
+      },
 }
 
-const RichTextComponent =({value}: ImageComponentProps) => {
-    return <PortableText value={value} components={components} />
+const RichTextComponent =({value}: ComponentProps) => {
+    return <PortableText value={value} components={components}  />
   }
 
 export default RichTextComponent;
