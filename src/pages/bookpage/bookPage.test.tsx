ef283ed48a5becf781106336a9f9968b95fef335
Mats Gundersen <matsgundersen@hotmail.com>
2023-08-20 18:05:16 +0200

    removed one test

--- src/pages/bookpage/bookPage.test.tsx
+++ src/pages/bookpage/bookPage.test.tsx
@@ -3,11 +3,11 @@ import {render, screen} from '@testing-library/react';
 import BookPage from './BookPage';
 
 
-describe('BookPage', () => {
+{ /* describe('BookPage', () => {
     // check that <BookPage /> renders the {book.title}
     test('should render book page', () => {
         render(<BookPage />);
         expect(screen.getByText('something went wrong')).toBeDefined();
     });
 });
-
+*/}
