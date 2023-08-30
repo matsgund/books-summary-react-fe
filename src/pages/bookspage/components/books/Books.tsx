a11b5ddb2218eec1b5bb3a6fdcafb6d38d2643f2
Mats Gundersen <matsgundersen@hotmail.com>
2023-08-30 19:34:14 +0200

    changed to infineteScrool component

--- src/pages/bookspage/components/books/Books.tsx
+++ src/pages/bookspage/components/books/Books.tsx
@@ -4,6 +4,8 @@ import BookCard from '@/shared-components/book-card/BookCard';
 import ErrorDisplayer from '@/shared-components/error-displayer/ErrorDisplayer';
 import { useEffect, useRef, useCallback, useState } from 'react';
 import LoadingSpinner from '@/shared-components/loading-spinner/LoadingSpinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import classes from './Books.module.css';
 
 import React from 'react';
 
@@ -12,51 +14,30 @@ const Books = () => {
    
   const [latestBookId, setLatestBookId] = useState<string>('');
   const { books, booksError, loading } = useBooks(latestBookId);
-  const observer = useRef<IntersectionObserver>();
-
-  const lastBookElementRef = useCallback((node: any) => {
-
-      if(booksError) return;
-      if(observer.current) observer.current.disconnect();
-
-      observer.current = new IntersectionObserver(entries => {
-          if(entries[0].isIntersecting) {
-              setLatestBookId(entries[0].target.id);
-          }    
-      });
-
-      if(node) observer.current.observe(node);
-
-  }, [latestBookId]);
 
   return (
     <>
-     
       {booksError && <ErrorDisplayer error="Unable to load books"/>}
-      {!booksError && books.length > 0 && 
-          books.map((item, i)=> {              
-              if(books.length === i +1) {
-                  return (
-                      <Link to={"/books/" + item.slug.current} key={i} ref={lastBookElementRef} id={item._id}>
-                          <BookCard
-                              key={i} 
-                              book={item}
-                              />
-                      </Link>
-                  )
-              } else {
-                  return (
-                      <Link to={"/books/" + item.slug.current} key={i}>
-                          <BookCard
-                              key={i} 
-                              book={item}
-                              />
-                      </Link>
-                  )
-              }                        
-          })
+      {!booksError && books.length > 0 &&
+      <InfiniteScroll
+            className={classes["books-main"]}
+            dataLength={books.length}
+            next={()=> setLatestBookId(books[books.length - 1]._id)}
+            hasMore={true}
+            loader={<LoadingSpinner isVisible={true}/>}
+            endMessage={<p style={{textAlign: 'center'}}>No more books to load</p>}>
+              
+            {books.map((item, i)=> (
+                  <Link to={"/books/" + item.slug.current} key={i} id={item._id}>
+                      <BookCard
+                          key={i} 
+                          book={item}
+                          />
+                  </Link>
+              ))}
+              
+        </InfiniteScroll>
         }
-        {loading && !booksError && <LoadingSpinner isVisible={loading}/>}
     </>
   );
 };
